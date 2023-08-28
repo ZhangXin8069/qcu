b12e5364379a41a197f71a686304c72cf9a5a0a7
zhangxin <zhangxin8069@qq.com>
2023-08-28 15:28:51 +0800

    2023-08-28-15-28-51

--- env.sh
+++ env.sh
@@ -1,13 +1,9 @@
 # init
 _HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
 echo 'HOME:'${_HOME}
 _NAME=$(basename "$0")
-name='test'
-work_name="test"
-tmp_name="tmp"
-work_path=${_HOME}/${work_name}
-tmp_path=${_HOME}/${tmp_name}
+SM_ARCH="sm_80"
+MAXRREGCOUNT="512"
 
 # source
 ## mkdir
@@ -20,9 +16,6 @@ mkdir ${_HOME}/tmp -p
 mkdir ${_HOME}/build -p
 mkdir ${_HOME}/doc -p
 
-# do
 ## export
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${_HOME}/lib/
 export PYTHONPATH=$(cd ~ && pwd)/external-libraries:$PYTHONPATH
-
-# done
