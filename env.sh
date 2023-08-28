790217673f8dddf3f8861e92b9d3da245e14fe34
zhangxin <zhangxin8069@qq.com>
2023-08-28 15:36:12 +0800

    2023-08-28-15-36-12

--- env.sh
+++ env.sh
@@ -10,11 +10,11 @@ MAXRREGCOUNT="512"
 mkdir ${_HOME}/bin -p
 mkdir ${_HOME}/include -p
 mkdir ${_HOME}/lib -p
-mkdir ${_HOME}/scripts -p
 mkdir ${_HOME}/test -p
 mkdir ${_HOME}/tmp -p
 mkdir ${_HOME}/build -p
 mkdir ${_HOME}/doc -p
+mkdir ${_HOME}/refer -p
 
 ## export
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${_HOME}/lib/
