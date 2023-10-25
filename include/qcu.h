b5d13041a50cada9ec49ea7688aa8cbccb3a2336
zhangxin <zhangxin8069@qq.com>
2023-10-25 22:43:01 +0800

    2023-10-25-22-43-01

--- include/qcu.h
+++ include/qcu.h
@@ -11,6 +11,7 @@ typedef struct QcuParam_s {
 
 void dslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity);
 void mpiDslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity, QcuParam *grid);
+void mpiCgQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity, QcuParam *grid);
 void testDslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity);
 
 #ifdef __cplusplus
