b86de0de1bc12446ea8d3d14a3fdb9b6d8c171b9
zhangxin <zhangxin8069@qq.com>
2024-01-02 22:07:02 +0800

    2024-01-02-22-07-02

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/aistudio/work/qcu
+# Install script for directory: /home/zhangxin/qcu
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/aistudio/work/qcu/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/zhangxin/qcu/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
