4a278a6a22f4c98162e20a9da3bc3b7702ada7d2
zhangxin <zhangxin8069@qq.com>
2024-02-25 10:43:45 +0800

    2024-02-25-10-43-45

--- python/pyqcu/wilson_dslash.py
+++ python/pyqcu/wilson_dslash.py
@@ -6,6 +6,10 @@ X, Y, Z, T = 0, 1, 2, 3
 BACKWARD, FORWARD = 0, 1
 
 
+def _Lxyzt(src: cupy.ndarray):
+    return src.shape[:-2][::-1]
+
+
 def eo_yzt(y: int, z: int, t: int) -> int:
     return (y + z + t) & 0x01
 
@@ -18,29 +22,24 @@ def move(DIM: int, WARD: int):
     pass
 
 
-def move_parity(param: dict, src: cupy.ndarray, WARD: int):
-    """
-    LatticeGauge [Nd, 2, Lt, Lz, Ly, Lx//2, Nc, Nc] (Nd == 4, 2 -> even-odd, Nc == 3)
-    LatticeFermion [2, Lt, Lz, Ly, Lx//2, Ns] (Ns == 4)
-    """
+"""
+LatticeGauge [Nd, 2, Lt, Lz, Ly, Lx//2, Nc, Nc] (Nd == 4, 2 -> even-odd, Nc == 3)
+LatticeFermion [2, Lt, Lz, Ly, Lx//2, Ns, Nc] (Ns == 4)
+"""
 
+
+def _run(src: cupy.ndarray, dest: cupy.ndarray, U: cupy.ndarray, parity: int):
+    Lxyzt = _Lxyzt(src=src)
+    print(Lxyzt)
     pass
 
 
-def _run(param: dict, src: cupy.ndarray, dest: cupy.ndarray, U: cupy.ndarray, parity: int):
-    pass
-
-
-def run(param: dict, src: cupy.ndarray, dest: cupy.ndarray, U: cupy.ndarray):
+def run(src: cupy.ndarray, dest: cupy.ndarray, U: cupy.ndarray):
     """
     param={'Lx':Lx, 'Ly':Ly, 'Lz':Lz, 'Lt':Lt}
     """
-    _src = move_parity(param=param, src=src, WARD=ODD)
-    _dest = move_parity(param=param, src=dest, WARD=EVEN)
-    _run(param=param, src=_src, dest=_dest, U=U, parity=EVEN)
-    _src = move_parity(param=param, src=src, WARD=EVEN)
-    _dest = move_parity(param=param, src=dest, WARD=ODD)
-    _run(param=param, src=_src, dest=_dest, U=U, parity=ODD)
+    _run(src=src[ODD], dest=dest[EVEN], U=U, parity=EVEN)
+    _run(src=src[EVEN], dest=dest[ODD], U=U, parity=ODD)
     pass
 
 
