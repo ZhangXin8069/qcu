ab4992dee7ce70ae36d00962e457311688194105
zhangxin <zhangxin8069@qq.com>
2024-03-20 23:08:36 +0800

    2024-03-20-23-08-36

--- refer/wjc_qcu/PyQuda/tests/test.nondebug_cg.py
+++ refer/wjc_qcu/PyQuda/tests/test.nondebug_cg.py
@@ -23,7 +23,7 @@ os.environ["QUDA_RESOURCE_PATH"] = ".cache"
 Nd, Ns, Nc = 4, 4, 3
 # latt_size = [Lx, Ly, Lz, Lt]
 
-#latt_size = [32, 32, 32, 32]
+#latt_size = [16, 16, 16, 32]
 #latt_size = [8, 8, 8, 8]
 #latt_size = [8, 8, 8, 8]
 latt_size = [16, 16, 16, 32]
