611efaa8df839c74550245d669f5fd9e949a01ee
zhangxin <zhangxin8069@qq.com>
2024-04-03 00:06:06 +0800

    2024-04-03-00-06-06

--- python/PyQuda/build/lib.linux-x86_64-cpython-310/pyquda/include/qcu/qcu.h
+++ python/PyQuda/build/lib.linux-x86_64-cpython-310/pyquda/include/qcu/qcu.h
@@ -8,9 +8,13 @@ typedef struct QcuParam_s {
   int lattice_size[4];
 } QcuParam;
 
-void dslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity);
-void mpiDslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity, QcuParam *grid);
-void testDslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param, int parity);
+void dslashQcu(void *fermion_out, void *fermion_in, void *gauge,
+               QcuParam *param, int parity);
+void dslashCloverQcu(void *fermion_out, void *fermion_in, void *gauge,
+                     QcuParam *param, int parity);
+void mpiDslashQcu(void *fermion_out, void *fermion_in, void *gauge,
+                  QcuParam *param, int parity, QcuParam *grid);
+void mpiBistabCgQcu(void *gauge, QcuParam *param, QcuParam *grid);
 
 #ifdef __cplusplus
 }
