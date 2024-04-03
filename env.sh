5333768b9871818dc3b2eed437f76d1bbed8ffa8
zhangxin <zhangxin8069@qq.com>
2024-04-03 15:59:24 +0800

    2024-04-03-15-59-24

--- env.sh
+++ env.sh
@@ -10,10 +10,13 @@ export PATH=$PATH:${_HOME}/bin
 MPI_HOME=/usr/local/openmpi
 export PATH=${MPI_HOME}/bin:$PATH
 export LD_LIBRARY_PATH=${MPI_HOME}/lib:$LD_LIBRARY_PATH
+export MPI_INCLUDE_PATH=${MPI_HOME}/include:$MPI_INCLUDE_PATH
 export MANPATH=${MPI_HOME}/share/man:$MANPATH
 ## cuda
 CUDA_HOME=/usr/local/cuda
 export PATH=${CUDA_HOME}/bin:$PATH
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib:$LD_LIBRARY_PATH
+export CUDA_INCLUDE_PATH=${CUDA_HOME}/include:$CUDA_INCLUDE_PATH
 export MANPATH=${CUDA_HOME}/share/man:$MANPATH
+
 export LD_LIBRARY_PATH="/usr/lib/wsl/lib:${LD_LIBRARY_PATH}" # wsl
