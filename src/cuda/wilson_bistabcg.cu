bf93bbb3bdcee36affd100ded868eda46bb7247d
zhangxin <zhangxin8069@qq.com>
2024-05-14 00:51:05 +0800

    2024-05-14-00-51-05

--- src/cuda/wilson_bistabcg.cu
+++ src/cuda/wilson_bistabcg.cu
@@ -2,20 +2,34 @@
 #include "../../include/qcu.h"
 #ifdef WILSON_BISTABCG
 
-//   for (int i = 0; i < lat_4dim12; i++) {
-//     b_e[i] =
-//         ans_e[i] - device_latt_tmp0[i] * kappa; // b_e=anw_e-kappa*D_eo(ans_o)
-//   }
-__global__ void wilson_bistabcg_func0(void *device_dest, int device_lat_x,
-                                         const int lat_4dim,
-                                         const int device_lat_z) {
+__global__ void wilson_bistabcg_func0(void *device_b_e, void *device_ans_e,
+                                      void *device_latt_tmp0, double kappa) {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
-lat_4dim 
-  LatticeComplex *origin_dest =
-      ((static_cast<LatticeComplex *>(device_dest)) +
-       t * lat_z * lat_y * lat_x * 12 + z * lat_y * lat_x * 12 +
-       y * lat_x * 12 + x * 12);
-  host_give_value(origin_dest, zero, 12);
+  LatticeComplex *b_e =
+      ((static_cast<LatticeComplex *>(device_b_e)) + index * LAT_SC);
+  LatticeComplex *ans_e =
+      ((static_cast<LatticeComplex *>(device_ans_e)) + index * LAT_SC);
+  LatticeComplex *latt_tmp0 =
+      ((static_cast<LatticeComplex *>(device_latt_tmp0)) + index * LAT_SC);
+  for (int i = 0; i < LAT_SC; i++) {
+    b_e[i] =
+        ans_e[i] - device_latt_tmp0[i] * kappa; // b_e=anw_e-kappa*D_eo(ans_o)
+  }
+}
+
+__global__ void wilson_bistabcg_func1(void *device_b_e, void *device_ans_e,
+                                      void *device_latt_tmp0, double kappa) {
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  LatticeComplex *b_e =
+      ((static_cast<LatticeComplex *>(device_b_e)) + index * LAT_SC);
+  LatticeComplex *ans_e =
+      ((static_cast<LatticeComplex *>(device_ans_e)) + index * LAT_SC);
+  LatticeComplex *latt_tmp0 =
+      ((static_cast<LatticeComplex *>(device_latt_tmp0)) + index * LAT_SC);
+  for (int i = 0; i < LAT_SC; i++) {
+    b_e[i] =
+        ans_e[i] - device_latt_tmp0[i] * kappa; // b_e=anw_e-kappa*D_eo(ans_o)
+  }
 }
 
 #endif
