aa8935bc45af62e81f807625c2f646174ef9eb0d
zhangxin <zhangxin8069@qq.com>
2024-05-16 23:59:48 +0800

    2024-05-16-23-59-48

--- src/cuda/qcu_mpi.cu
+++ src/cuda/qcu_mpi.cu
@@ -6,15 +6,13 @@ __global__ void wilson_bistabcg_part_dot(void *device_dot_tmp,
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   int lat_4dim = gridDim.x * blockDim.x;
   if (idx < lat_4dim) {
-    LatticeComplex *device_dot_tmp =
-        static_cast<LatticeComplex *>(device_dot_tmp);
+    LatticeComplex *dot_tmp = static_cast<LatticeComplex *>(device_dot_tmp);
     LatticeComplex *val0 = static_cast<LatticeComplex *>(device_val0);
     LatticeComplex *val1 = static_cast<LatticeComplex *>(device_val1);
-    device_dot_tmp[idx].real = 0.0;
-    device_dot_tmp[idx].imag = 0.0;
+    dot_tmp[idx].real = 0.0;
+    dot_tmp[idx].imag = 0.0;
     for (int i = 0; i < LAT_SC; ++i) {
-      device_dot_tmp[idx] +=
-          val0[idx * LAT_SC + i].conj() * val1[idx * LAT_SC + i];
+      dot_tmp[idx] += val0[idx * LAT_SC + i].conj() * val1[idx * LAT_SC + i];
     }
   }
 }
