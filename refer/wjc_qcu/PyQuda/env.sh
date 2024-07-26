96b7e8c6d165ba5b52cf6df5920aefcc1eca375b
zhangxin <zhangxin8069@qq.com>
2024-07-26 12:02:54 +0800

    2024-07-26-12-02-54

--- refer/wjc_qcu/PyQuda/env.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=$(pwd)/lib/:$LD_LIBRARY_PATH
-#export PYTHONPATH=/home/aistudio/external-libraries/:$PYTHONPATH
--- refer/wjc_qcu/PyQuda/tests/run_new_cg.sh
+++
@@ -1,2 +0,0 @@
-echo "mpiexec -np 1 python test.dslash.mpi.py"
-mpiexec -n 1 python test.nondebug_cg.py
