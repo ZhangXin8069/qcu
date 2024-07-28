4d31551d415ec93c20b21324a70671f50b17a866
zhangxin <zhangxin8069@qq.com>
2024-07-28 17:12:05 +0800

    2024-07-28-17-12-05

--- test/bml.test.nccl-wjc.sh
+++
@@ -1,3 +0,0 @@
-nvcc -o test.nccl-wjc test.nccl-wjc.cu -arch=sm_70 -lnccl -I/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi -L/usr/lib/x86_64-linux-gnu/openmpi/lib -lmpi -I/usr/lib/x86_64-linux-gnu/openmpi/include
-
-nvprof --profile-child-processes -f -o log.nvvp%p mpirun -np 1 test.nccl-wjc
