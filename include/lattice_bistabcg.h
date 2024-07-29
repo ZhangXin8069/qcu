dff1326bb0fcf9f93888386226c6dbd651d3b931
zhangxin <zhangxin8069@qq.com>
2024-07-29 14:44:04 +0800

    2024-07-29-14-44-04

--- include/lattice_bistabcg.h
+++ include/lattice_bistabcg.h
@@ -1,38 +1,10 @@
 #ifndef _LATTICE_BISTABCG_H
 #define _LATTICE_BISTABCG_H
+#include "./bistabcg.h"
+#include "./dslash.h"
 #include "./lattice_cuda.h"
 #include "./lattice_dslash.h"
 #include "./lattice_mpi.h"
-
-// #define DEBUG_NCCL_WILSON_BISTABCG
-
-__global__ void bistabcg_give_b_e(void *device_b_e, void *device_ans_e,
-                                  void *device_tmps0, double kappa);
-
-__global__ void bistabcg_give_b_o(void *device_b_o, void *device_ans_o,
-                                  void *device_tmps1, double kappa);
-
-__global__ void bistabcg_give_b__0(void *device_b__o, void *device_b_o,
-                                   void *device_tmps0, double kappa);
-
-__global__ void bistabcg_give_dest_o(void *device_dest_o, void *device_src_o,
-                                     void *device_tmps1, double kappa);
-
-__global__ void bistabcg_give_rr(void *device_r, void *device_b__o,
-                                 void *device_r_tilde);
-
-__global__ void bistabcg_give_p(void *device_p, void *device_r, void *device_v,
-                                LatticeComplex omega, LatticeComplex beta);
-
-__global__ void bistabcg_give_s(void *device_s, void *device_r, void *device_v,
-                                LatticeComplex alpha);
-
-__global__ void bistabcg_give_x_o(void *device_x_o, void *device_p,
-                                  void *device_s, LatticeComplex alpha,
-                                  LatticeComplex omega);
-
-__global__ void bistabcg_give_r(void *device_r, void *device_s, void *device_tt,
-                                LatticeComplex omega);
 
 struct LatticeBistabcg {
   LatticeSet *set_ptr;
