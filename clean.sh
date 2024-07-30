9f946d4c62154dc347b43dd4e9d3133d01710cc5
zhangxin <zhangxin8069@qq.com>
2024-07-31 01:30:35 +0800

    2024-07-31-01-30-35

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@ rm -rf CMakeFiles
 rm cmake_install.cmake
 rm CMakeCache.txt
 rm Makefile
-rm '"clangd.format.tabSize": 4'
+git rm '"clangd.format.tabSize": 4'
 rm -rf .ipynb_checkpoints
 # rm ./test/.cache/*
 git rm .gitmodules
