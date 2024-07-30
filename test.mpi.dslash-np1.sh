416bef98cfaf1df105a863cc25a786337dcbf080
zhangxin <zhangxin8069@qq.com>
2024-07-31 00:28:13 +0800

    2024-07-31-00-28-13

--- test.mpi.dslash-np1.sh
+++
@@ -1,4 +0,0 @@
-bash ./make.sh
-pushd ./test
-nvprof --profile-child-processes -f -o log.nvvp%p mpirun -n 1 python ./test.mpi.dslash.qcu-np1.py
-popd
--- test.mpi.dslash-np4.sh
+++
@@ -1,4 +0,0 @@
-bash ./make.sh
-pushd ./test
-nvprof --profile-child-processes -f -o log.nvvp%p mpirun -n 4 python ./test.mpi.dslash.qcu-np4.py
-popd
