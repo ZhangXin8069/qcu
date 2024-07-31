cbfac3d04bb123dd95ccff26de992a937a8ac5d9
zhangxin <zhangxin8069@qq.com>
2024-08-01 03:20:30 +0800

    2024-08-01-03-20-30

--- src/bistabcg.cu
+++ src/bistabcg.cu
@@ -53,131 +53,201 @@ __global__ void bistabcg_give_1diff(void *device_vals) {
 __global__ void bistabcg_give_b_e(void *device_b_e, void *device_ans_e,
                                   void *device_vec0, double kappa) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *b_e = static_cast<LatticeComplex *>(device_b_e);
-  LatticeComplex *ans_e = static_cast<LatticeComplex *>(device_ans_e);
-  LatticeComplex *latt_tmp0 = static_cast<LatticeComplex *>(device_vec0);
+  LatticeComplex *origin_b_e =
+      (static_cast<LatticeComplex *>(device_b_e) + idx * _LAT_SC_);
+  LatticeComplex *origin_ans_e =
+      (static_cast<LatticeComplex *>(device_ans_e) + idx * _LAT_SC_);
+  LatticeComplex *origin_vec0 =
+      (static_cast<LatticeComplex *>(device_vec0) + idx * _LAT_SC_);
+  LatticeComplex b_e[_LAT_SC_];
+  LatticeComplex ans_e[_LAT_SC_];
+  LatticeComplex vec0[_LAT_SC_];
+  give_ptr(ans_e, origin_ans_e, _LAT_SC_);
+  give_ptr(vec0, origin_vec0, _LAT_SC_);
   for (int i = 0; i < _LAT_SC_; ++i) {
-    b_e[idx * _LAT_SC_ + i] =
-        ans_e[idx * _LAT_SC_ + i] -
-        latt_tmp0[idx * _LAT_SC_ + i] * kappa; // b_e=ans_e-kappa*D_eo(ans_o)
+    b_e[i] = ans_e[i] - vec0[i] * kappa; // b_e=ans_e-kappa*D_eo(ans_o)
   }
+  give_ptr(origin_b_e, b_e, _LAT_SC_);
 }
 
 __global__ void bistabcg_give_b_o(void *device_b_o, void *device_ans_o,
                                   void *device_vec1, double kappa) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *b_o = static_cast<LatticeComplex *>(device_b_o);
-  LatticeComplex *ans_o = static_cast<LatticeComplex *>(device_ans_o);
-  LatticeComplex *latt_tmp1 = static_cast<LatticeComplex *>(device_vec1);
+  LatticeComplex *origin_b_o =
+      (static_cast<LatticeComplex *>(device_b_o) + idx * _LAT_SC_);
+  LatticeComplex *origin_ans_o =
+      (static_cast<LatticeComplex *>(device_ans_o) + idx * _LAT_SC_);
+  LatticeComplex *origin_vec1 =
+      (static_cast<LatticeComplex *>(device_vec1) + idx * _LAT_SC_);
+  LatticeComplex b_o[_LAT_SC_];
+  LatticeComplex ans_o[_LAT_SC_];
+  LatticeComplex vec1[_LAT_SC_];
+  give_ptr(ans_o, origin_ans_o, _LAT_SC_);
+  give_ptr(vec1, origin_vec1, _LAT_SC_);
   for (int i = 0; i < _LAT_SC_; ++i) {
-    b_o[idx * _LAT_SC_ + i] =
-        ans_o[idx * _LAT_SC_ + i] -
-        latt_tmp1[idx * _LAT_SC_ + i] * kappa; // b_o=ans_o-kappa*D_oe(ans_e)
+    b_o[i] = ans_o[i] - vec1[i] * kappa; // b_o=ans_o-kappa*D_oe(ans_e)
   }
+  give_ptr(origin_b_o, b_o, _LAT_SC_);
 }
 
 __global__ void bistabcg_give_b__0(void *device_b__o, void *device_b_o,
                                    void *device_vec0, double kappa) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *b__o = static_cast<LatticeComplex *>(device_b__o);
-  LatticeComplex *b_o = static_cast<LatticeComplex *>(device_b_o);
-  LatticeComplex *latt_tmp0 = static_cast<LatticeComplex *>(device_vec0);
+  LatticeComplex *origin_b__o =
+      (static_cast<LatticeComplex *>(device_b__o) + idx * _LAT_SC_);
+  LatticeComplex *origin_b_o =
+      (static_cast<LatticeComplex *>(device_b_o) + idx * _LAT_SC_);
+  LatticeComplex *origin_vec0 =
+      (static_cast<LatticeComplex *>(device_vec0) + idx * _LAT_SC_);
+  LatticeComplex b__o[_LAT_SC_];
+  LatticeComplex b_o[_LAT_SC_];
+  LatticeComplex vec0[_LAT_SC_];
+  give_ptr(b_o, origin_b_o, _LAT_SC_);
+  give_ptr(vec0, origin_vec0, _LAT_SC_);
   for (int i = 0; i < _LAT_SC_; ++i) {
-    b__o[idx * _LAT_SC_ + i] =
-        b_o[idx * _LAT_SC_ + i] +
-        latt_tmp0[idx * _LAT_SC_ + i] * kappa; // b__o=b_o+kappa*D_oe(b_e)
+    b__o[i] = b_o[i] + vec0[i] * kappa; // b__o=b_o+kappa*D_oe(b_e)
   }
+  give_ptr(origin_b__o, b__o, _LAT_SC_);
 }
 
 __global__ void bistabcg_give_dest_o(void *device_dest_o, void *device_src_o,
                                      void *device_vec1, double kappa) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *dest_o = static_cast<LatticeComplex *>(device_dest_o);
-  LatticeComplex *src_o = static_cast<LatticeComplex *>(device_src_o);
-  LatticeComplex *latt_tmp1 = static_cast<LatticeComplex *>(device_vec1);
+  LatticeComplex *origin_dest_o =
+      (static_cast<LatticeComplex *>(device_dest_o) + idx * _LAT_SC_);
+  LatticeComplex *origin_src_o =
+      (static_cast<LatticeComplex *>(device_src_o) + idx * _LAT_SC_);
+  LatticeComplex *origin_vec1 =
+      (static_cast<LatticeComplex *>(device_vec1) + idx * _LAT_SC_);
+  LatticeComplex dest_o[_LAT_SC_];
+  LatticeComplex src_o[_LAT_SC_];
+  LatticeComplex vec1[_LAT_SC_];
+  give_ptr(src_o, origin_src_o, _LAT_SC_);
+  give_ptr(vec1, origin_vec1, _LAT_SC_);
   for (int i = 0; i < _LAT_SC_; ++i) {
-    dest_o[idx * _LAT_SC_ + i] =
-        src_o[idx * _LAT_SC_ + i] - latt_tmp1[idx * _LAT_SC_ + i] * kappa *
-                                        kappa; // dest_o=ans_o-kappa^2*tmp1
+    dest_o[i] = src_o[i] - vec1[i] * kappa * kappa; // dest_o=ans_o-kappa^2*tmp1
   }
+  give_ptr(origin_dest_o, dest_o, _LAT_SC_);
 }
 
 __global__ void bistabcg_give_rr(void *device_r, void *device_b__o,
                                  void *device_r_tilde) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *r = static_cast<LatticeComplex *>(device_r);
-  LatticeComplex *b__o = static_cast<LatticeComplex *>(device_b__o);
-  LatticeComplex *r_tilde = static_cast<LatticeComplex *>(device_r_tilde);
+  LatticeComplex *origin_r =
+      (static_cast<LatticeComplex *>(device_r) + idx * _LAT_SC_);
+  LatticeComplex *origin_b__o =
+      (static_cast<LatticeComplex *>(device_b__o) + idx * _LAT_SC_);
+  LatticeComplex *origin_r_tilde =
+      (static_cast<LatticeComplex *>(device_r_tilde) + idx * _LAT_SC_);
+  LatticeComplex r[_LAT_SC_];
+  LatticeComplex b__o[_LAT_SC_];
+  LatticeComplex r_tilde[_LAT_SC_];
+  give_ptr(r, origin_r, _LAT_SC_);
+  give_ptr(b__o, origin_b__o, _LAT_SC_);
   for (int i = 0; i < _LAT_SC_; ++i) {
-    r[idx * _LAT_SC_ + i] = b__o[idx * _LAT_SC_ + i] - r[idx * _LAT_SC_ + i];
-    r_tilde[idx * _LAT_SC_ + i] = r[idx * _LAT_SC_ + i];
+    r[i] = b__o[i] - r[i];
+    r_tilde[i] = r[i];
   }
+  give_ptr(origin_r, r, _LAT_SC_);
+  give_ptr(origin_r_tilde, r_tilde, _LAT_SC_);
 }
 
 __global__ void bistabcg_give_p(void *device_p, void *device_r, void *device_v,
                                 void *device_vals) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *p = static_cast<LatticeComplex *>(device_p);
-  LatticeComplex *r = static_cast<LatticeComplex *>(device_r);
-  LatticeComplex *v = static_cast<LatticeComplex *>(device_v);
+  LatticeComplex *origin_p =
+      (static_cast<LatticeComplex *>(device_p) + idx * _LAT_SC_);
+  LatticeComplex *origin_r =
+      (static_cast<LatticeComplex *>(device_r) + idx * _LAT_SC_);
+  LatticeComplex *origin_v =
+      (static_cast<LatticeComplex *>(device_v) + idx * _LAT_SC_);
+  LatticeComplex p[_LAT_SC_];
+  LatticeComplex r[_LAT_SC_];
+  LatticeComplex v[_LAT_SC_];
+  give_ptr(p, origin_p, _LAT_SC_);
+  give_ptr(r, origin_r, _LAT_SC_);
+  give_ptr(v, origin_v, _LAT_SC_);
   LatticeComplex *origin_vals = static_cast<LatticeComplex *>(device_vals);
   LatticeComplex beta;
   beta = origin_vals[_beta_];
   LatticeComplex omega;
   omega = origin_vals[_omega_];
   for (int i = 0; i < _LAT_SC_; ++i) {
-    p[idx * _LAT_SC_ + i] =
-        r[idx * _LAT_SC_ + i] +
-        (p[idx * _LAT_SC_ + i] - v[idx * _LAT_SC_ + i] * omega) * beta;
+    p[i] = r[i] + (p[i] - v[i] * omega) * beta;
   }
+  give_ptr(origin_p, p, _LAT_SC_);
 }
 
 __global__ void bistabcg_give_s(void *device_s, void *device_r, void *device_v,
                                 void *device_vals) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *s = static_cast<LatticeComplex *>(device_s);
-  LatticeComplex *r = static_cast<LatticeComplex *>(device_r);
-  LatticeComplex *v = static_cast<LatticeComplex *>(device_v);
+  LatticeComplex *origin_s =
+      (static_cast<LatticeComplex *>(device_s) + idx * _LAT_SC_);
+  LatticeComplex *origin_r =
+      (static_cast<LatticeComplex *>(device_r) + idx * _LAT_SC_);
+  LatticeComplex *origin_v =
+      (static_cast<LatticeComplex *>(device_v) + idx * _LAT_SC_);
+  LatticeComplex s[_LAT_SC_];
+  LatticeComplex r[_LAT_SC_];
+  LatticeComplex v[_LAT_SC_];
+  give_ptr(r, origin_r, _LAT_SC_);
+  give_ptr(v, origin_v, _LAT_SC_);
   LatticeComplex *origin_vals = static_cast<LatticeComplex *>(device_vals);
   LatticeComplex alpha;
   alpha = origin_vals[_alpha_];
   for (int i = 0; i < _LAT_SC_; ++i) {
-    s[idx * _LAT_SC_ + i] =
-        r[idx * _LAT_SC_ + i] - v[idx * _LAT_SC_ + i] * alpha;
+    s[i] = r[i] - v[i] * alpha;
   }
+  give_ptr(origin_s, s, _LAT_SC_);
 }
 
 __global__ void bistabcg_give_x_o(void *device_x_o, void *device_p,
                                   void *device_s, void *device_vals) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *x_o = static_cast<LatticeComplex *>(device_x_o);
-  LatticeComplex *p = static_cast<LatticeComplex *>(device_p);
-  LatticeComplex *s = static_cast<LatticeComplex *>(device_s);
+  LatticeComplex *origin_x_o =
+      (static_cast<LatticeComplex *>(device_x_o) + idx * _LAT_SC_);
+  LatticeComplex *origin_p =
+      (static_cast<LatticeComplex *>(device_p) + idx * _LAT_SC_);
+  LatticeComplex *origin_s =
+      (static_cast<LatticeComplex *>(device_s) + idx * _LAT_SC_);
+  LatticeComplex x_o[_LAT_SC_];
+  LatticeComplex p[_LAT_SC_];
+  LatticeComplex s[_LAT_SC_];
+  give_ptr(x_o, origin_x_o, _LAT_SC_);
+  give_ptr(p, origin_p, _LAT_SC_);
+  give_ptr(s, origin_s, _LAT_SC_);
   LatticeComplex *origin_vals = static_cast<LatticeComplex *>(device_vals);
   LatticeComplex alpha;
   alpha = origin_vals[_alpha_];
   LatticeComplex omega;
   omega = origin_vals[_omega_];
   for (int i = 0; i < _LAT_SC_; ++i) {
-    x_o[idx * _LAT_SC_ + i] = x_o[idx * _LAT_SC_ + i] +
-                              p[idx * _LAT_SC_ + i] * alpha +
-                              s[idx * _LAT_SC_ + i] * omega;
+    x_o[i] = x_o[i] + p[i] * alpha + s[i] * omega;
   }
+  give_ptr(origin_x_o, x_o, _LAT_SC_);
 }
 
 __global__ void bistabcg_give_r(void *device_r, void *device_s, void *device_tt,
                                 void *device_vals) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  LatticeComplex *r = static_cast<LatticeComplex *>(device_r);
-  LatticeComplex *s = static_cast<LatticeComplex *>(device_s);
-  LatticeComplex *t = static_cast<LatticeComplex *>(device_tt);
+  LatticeComplex *origin_r =
+      (static_cast<LatticeComplex *>(device_r) + idx * _LAT_SC_);
+  LatticeComplex *origin_s =
+      (static_cast<LatticeComplex *>(device_s) + idx * _LAT_SC_);
+  LatticeComplex *origin_t =
+      (static_cast<LatticeComplex *>(device_tt) + idx * _LAT_SC_);
+  LatticeComplex r[_LAT_SC_];
+  LatticeComplex s[_LAT_SC_];
+  LatticeComplex t[_LAT_SC_];
+  give_ptr(s, origin_s, _LAT_SC_);
+  give_ptr(t, origin_t, _LAT_SC_);
   LatticeComplex *origin_vals = static_cast<LatticeComplex *>(device_vals);
   LatticeComplex omega;
   omega = origin_vals[_omega_];
   for (int i = 0; i < _LAT_SC_; ++i) {
-    r[idx * _LAT_SC_ + i] =
-        s[idx * _LAT_SC_ + i] - t[idx * _LAT_SC_ + i] * omega;
+    r[i] = s[i] - t[i] * omega;
   }
+  give_ptr(origin_r, r, _LAT_SC_);
 }
 
 #endif
