f9e9c53d4b7f6be8e29c99e157e2803bf95e3a30
zhangxin <zhangxin8069@qq.com>
2024-08-07 22:35:11 +0800

    2024-08-07-22-35-11

--- test.nccl.bistabcg-np1.sh
+++ test.nccl.bistabcg-np1.sh
@@ -1,5 +1,4 @@
 bash ./make.sh
 pushd ./test
-rm log_*
-nvprof --profile-child-processes -f -o log_%h_%p.nvvp mpirun -n 1 python ./test.nccl.bistabcg.qcu-np1.py
+mpirun -n 1 python ./test.nccl.bistabcg.qcu-np1.py
 popd
--- test.nccl.dslash-np4.sh
+++ test.nccl.dslash-np4.sh
@@ -1,5 +1,4 @@
 bash ./make.sh
 pushd ./test
-rm log_*
-nvprof --profile-child-processes -f -o log_%h_%p.nvvp mpirun -n 4 python ./test.nccl.dslash.qcu-np4.py
+mpirun -n 4 python ./test.nccl.dslash.qcu-np4.py
 popd
