43024ceebc4dcc957f063a92af72ae93b5186336
zhangxin <zhangxin8069@qq.com>
2024-08-09 14:30:02 +0800

    2024-08-09-14-30-02

--- include/lattice_dslash.h
+++ include/lattice_dslash.h
@@ -49,17 +49,18 @@ struct LatticeWilsonDslash {
         ncclSend(set_ptr->device_send_vec[_B_X_], set_ptr->lat_3dim_SC[_X_],
                  ncclDouble, set_ptr->move_wards[_B_X_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_X_]);
-        ncclSend(set_ptr->device_send_vec[_F_X_], set_ptr->lat_3dim_SC[_X_],
+        ncclRecv(set_ptr->device_recv_vec[_F_X_], set_ptr->lat_3dim_SC[_X_],
                  ncclDouble, set_ptr->move_wards[_F_X_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_X_]);
         ncclGroupEnd();
         ncclGroupStart();
+        ncclSend(set_ptr->device_send_vec[_F_X_], set_ptr->lat_3dim_SC[_X_],
+                 ncclDouble, set_ptr->move_wards[_F_X_], set_ptr->nccl_comm,
+                 set_ptr->stream_dims[_X_]);
         ncclRecv(set_ptr->device_recv_vec[_B_X_], set_ptr->lat_3dim_SC[_X_],
                  ncclDouble, set_ptr->move_wards[_B_X_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_X_]);
-        ncclRecv(set_ptr->device_recv_vec[_F_X_], set_ptr->lat_3dim_SC[_X_],
-                 ncclDouble, set_ptr->move_wards[_F_X_], set_ptr->nccl_comm,
-                 set_ptr->stream_dims[_X_]);
+
         ncclGroupEnd();
       }
     }
@@ -79,16 +80,18 @@ struct LatticeWilsonDslash {
         ncclSend(set_ptr->device_send_vec[_B_Y_], set_ptr->lat_3dim_SC[_Y_],
                  ncclDouble, set_ptr->move_wards[_B_Y_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_Y_]);
-        ncclSend(set_ptr->device_send_vec[_F_Y_], set_ptr->lat_3dim_SC[_Y_],
+
+        ncclRecv(set_ptr->device_recv_vec[_F_Y_], set_ptr->lat_3dim_SC[_Y_],
                  ncclDouble, set_ptr->move_wards[_F_Y_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_Y_]);
         ncclGroupEnd();
         ncclGroupStart();
+
+        ncclSend(set_ptr->device_send_vec[_F_Y_], set_ptr->lat_3dim_SC[_Y_],
+                 ncclDouble, set_ptr->move_wards[_F_Y_], set_ptr->nccl_comm,
+                 set_ptr->stream_dims[_Y_]);
         ncclRecv(set_ptr->device_recv_vec[_B_Y_], set_ptr->lat_3dim_SC[_Y_],
                  ncclDouble, set_ptr->move_wards[_B_Y_], set_ptr->nccl_comm,
-                 set_ptr->stream_dims[_Y_]);
-        ncclRecv(set_ptr->device_recv_vec[_F_Y_], set_ptr->lat_3dim_SC[_Y_],
-                 ncclDouble, set_ptr->move_wards[_F_Y_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_Y_]);
         ncclGroupEnd();
       }
@@ -109,16 +112,16 @@ struct LatticeWilsonDslash {
         ncclSend(set_ptr->device_send_vec[_B_Z_], set_ptr->lat_3dim_SC[_Z_],
                  ncclDouble, set_ptr->move_wards[_B_Z_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_Z_]);
-        ncclSend(set_ptr->device_send_vec[_F_Z_], set_ptr->lat_3dim_SC[_Z_],
+        ncclRecv(set_ptr->device_recv_vec[_F_Z_], set_ptr->lat_3dim_SC[_Z_],
                  ncclDouble, set_ptr->move_wards[_F_Z_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_Z_]);
         ncclGroupEnd();
         ncclGroupStart();
+        ncclSend(set_ptr->device_send_vec[_F_Z_], set_ptr->lat_3dim_SC[_Z_],
+                 ncclDouble, set_ptr->move_wards[_F_Z_], set_ptr->nccl_comm,
+                 set_ptr->stream_dims[_Z_]);
         ncclRecv(set_ptr->device_recv_vec[_B_Z_], set_ptr->lat_3dim_SC[_Z_],
                  ncclDouble, set_ptr->move_wards[_B_Z_], set_ptr->nccl_comm,
-                 set_ptr->stream_dims[_Z_]);
-        ncclRecv(set_ptr->device_recv_vec[_F_Z_], set_ptr->lat_3dim_SC[_Z_],
-                 ncclDouble, set_ptr->move_wards[_F_Z_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_Z_]);
         ncclGroupEnd();
       }
@@ -139,16 +142,16 @@ struct LatticeWilsonDslash {
         ncclSend(set_ptr->device_send_vec[_B_T_], set_ptr->lat_3dim_SC[_T_],
                  ncclDouble, set_ptr->move_wards[_B_T_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_T_]);
-        ncclSend(set_ptr->device_send_vec[_F_T_], set_ptr->lat_3dim_SC[_T_],
+        ncclRecv(set_ptr->device_recv_vec[_F_T_], set_ptr->lat_3dim_SC[_T_],
                  ncclDouble, set_ptr->move_wards[_F_T_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_T_]);
         ncclGroupEnd();
         ncclGroupStart();
+        ncclSend(set_ptr->device_send_vec[_F_T_], set_ptr->lat_3dim_SC[_T_],
+                 ncclDouble, set_ptr->move_wards[_F_T_], set_ptr->nccl_comm,
+                 set_ptr->stream_dims[_T_]);
         ncclRecv(set_ptr->device_recv_vec[_B_T_], set_ptr->lat_3dim_SC[_T_],
                  ncclDouble, set_ptr->move_wards[_B_T_], set_ptr->nccl_comm,
-                 set_ptr->stream_dims[_T_]);
-        ncclRecv(set_ptr->device_recv_vec[_F_T_], set_ptr->lat_3dim_SC[_T_],
-                 ncclDouble, set_ptr->move_wards[_F_T_], set_ptr->nccl_comm,
                  set_ptr->stream_dims[_T_]);
         ncclGroupEnd();
       }
