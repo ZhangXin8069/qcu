4578a94b5c9fbce54b90539dd203d33b2f9ba320
zhangxin <zhangxin8069@qq.com>
2024-08-16 15:26:08 +0800

    2024-08-16-15-26-08

--- snsc-env.sh
+++ snsc-env.sh
@@ -1,1 +1,8 @@
-# there is nothing here!
+# init
+_HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+echo 'HOME:'${_HOME}
+# export
+## zhangxin
+export LD_LIBRARY_PATH=${_HOME}/lib:$LD_LIBRARY_PATH # if any
+export TERM=xterm-256color
+export PYTHONPATH=${_HOME}/lib:${PYTHONPATH}
