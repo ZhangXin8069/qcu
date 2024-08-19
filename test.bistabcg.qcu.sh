34d41f9bb576215a06833ec85404391b138816ea
zhangxin <zhangxin8069@qq.com>
2024-08-19 23:47:03 +0800

    2024-08-19-23-47-03

--- test.bistabcg.qcu.sh
+++
@@ -1,6 +0,0 @@
-bash ./make.sh
-pushd ./test
-rm log_*
-rm log_*
-mpirun -n 1 python ./test.bistabcg.qcu.py
-popd
--- test.wilson.dslash.sh
+++
@@ -1,6 +0,0 @@
-bash ./make.sh
-pushd ./test
-rm log_*
-rm log_*
-mpirun -n 1 python ./test.wilson.dslash.qcu.py
-popd
