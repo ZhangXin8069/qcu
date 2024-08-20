d293f52ea6fb24297ddfd974db5f9bf73296b598
zhangxin <zhangxin8069@qq.com>
2024-08-20 16:46:43 +0800

    2024-08-20-16-46-43

--- snsc-env.sh
+++ snsc-env.sh
@@ -5,3 +5,6 @@ echo 'HOME:'${_HOME}
 ## zhangxin
 export LD_LIBRARY_PATH=${_HOME}/lib:$LD_LIBRARY_PATH # if any
 export PYTHONPATH=${_HOME}/lib:${PYTHONPATH}
+## quda
+export QUDA_ENABLE_P2P=0
+export QUDA_ENABLE_TUNING=0
