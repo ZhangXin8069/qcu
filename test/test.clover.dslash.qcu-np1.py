5cbd665326072422dfd8a5c3da562f024bd24a32
zhangxin <zhangxin8069@qq.com>
2024-08-31 13:43:59 +0800

    2024-08-31-13-43-59

--- test/test.clover.dslash.qcu-np1.py
+++ test/test.clover.dslash.qcu-np1.py
@@ -11,7 +11,7 @@ test_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(test_dir, ".."))
 os.environ["QUDA_RESOURCE_PATH"] = ".cache"
 # latt_size = [32, 32, 32, 32]
-# latt_size = [32, 32, 32, 64]
+# latt_size = [16, 16, 16, 32]
 # latt_size = [16, 16, 16, 16]
 latt_size = [16, 16, 16, 32]
 # latt_size = [8, 16, 16, 16]
