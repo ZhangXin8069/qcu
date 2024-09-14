d0ec3c908b6aa160314a5a110369297bdc5157b8
zhangxin <zhangxin8069@qq.com>
2024-09-14 15:30:28 +0800

    2024-09-14-15-30-28

--- test.clover.dslash.qcu-np2.sh
+++ test.clover.dslash.qcu-np2.sh
@@ -2,4 +2,5 @@ bash ./make.sh
 pushd ./test
 rm log_*
 mpirun -n 2 python ./test.clover.dslash.qcu-np2.py
+# nsys profile -f true -o log_%h_%p mpirun -n 2 python ./test.clover.dslash.qcu-np2.py
 popd
