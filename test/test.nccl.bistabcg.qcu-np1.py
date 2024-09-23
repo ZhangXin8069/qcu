b5247b9f2a7932db1022f14e5633d20d28b8429a
zhangxin <zhangxin8069@qq.com>
2024-09-23 13:08:05 +0800

    2024-09-23-13-08-04

--- test/test.nccl.bistabcg.qcu-np1.py
+++ test/test.nccl.bistabcg.qcu-np1.py
@@ -49,7 +49,7 @@ dslash = core.getDslash(
     anti_periodic_t=False,
 )
 # dslash.invert_param.inv_type = 0  # QUDA_CG_INVERTER
-dslash.invert_param.inv_type = 1  # QUDA_BICGSTAB_INVERTER
+# dslash.invert_param.inv_type = 1  # QUDA_BICGSTAB_INVERTER
 U = gauge_utils.gaussGauge(latt_size, 0)
 dslash.loadGauge(U)
 
