1d7e02a7a559904b9f4f4344d2fd1e22418f2625
zhangxin <zhangxin8069@qq.com>
2024-10-19 22:41:58 +0800

    2024-10-19-22-41-58

--- computer-env.sh
+++ computer-env.sh
@@ -3,6 +3,7 @@ _HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo 'HOME:'${_HOME}
 # # export
 # ## zhangxin
+export LD_LIBRARY_PATH=${HOME}/miniconda/envs/qcu/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=${_HOME}/lib:$LD_LIBRARY_PATH # if any
 # export TERM=xterm-256color
 # # export PYTHONPATH=${HOME}/.local/lib/python3.10/site-packages # wsl
