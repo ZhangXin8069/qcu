bef9bd3f106c740970a73d7cc8d0c4544fdf952c
zhangxin <zhangxin8069@qq.com>
2024-11-11 04:51:20 +0000

    2024-11-11-04-51-20

--- test.nccl.bistabcg-np2.sh
+++ test.nccl.bistabcg-np2.sh
@@ -1,6 +1,6 @@
 bash ./make.sh
 pushd ./test
 rm log_*
-nsys profile -f true -o log_%h_%p mpirun -n 2 python ./test.nccl.bistabcg.qcu-np2.py
+mpirun -n 2 python ./test.nccl.bistabcg.qcu-np2.py
 popd
 
