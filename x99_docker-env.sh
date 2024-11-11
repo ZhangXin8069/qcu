c7760c62909ec90c42c0b588f80f556f60c4840f
zhangxin <zhangxin8069@qq.com>
2024-11-12 00:52:05 +0800

    2024-11-12-00-52-05

--- x99_docker-env.sh
+++ x99_docker-env.sh
@@ -12,8 +12,7 @@ export QUDA_ENABLE_TUNING=0
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 ## nccl
-# export NCCL_DEBUG=
-export NCCL_DEBUG=INFO
+export NCCL_DEBUG=
+# export NCCL_DEBUG=INFO
 # cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g'
 export NCCL_SOCKET_IFNAME=eth0
-export NCCL_IB_DISABLE=1
