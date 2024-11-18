86241ecba9da1bf717b9021d917cac80bd739419
zhangxin <zhangxin8069@qq.com>
2024-11-18 16:41:09 +0800

    2024-11-18-16-41-08

--- ssub.sh
+++ ssub.sh
@@ -3,12 +3,16 @@
 #SBATCH --partition=na100-ins
 #SBATCH --nodes=1
 #SBATCH -n 8
-#SBATCH --time=00-00:30:00
 #SBATCH --output=ssub.out
 #SBATCH --error=ssub.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zhangxin8069@qq.com
 #SBATCH --gres=gpu:8
 #SBATCH -w "gpu025"
-source /public/home/zhangxin/env.sh
-bash ./test.clover.dslash.qcu-np8.sh
+# source /public/home/zhangxin/env.sh
+# bash ./make.sh
+# pushd ./test
+# rm log_*
+# mpirun -n 8 python ./test.nccl.bistabcg.qcu-np8.py
+# popd
+sleep 5000
