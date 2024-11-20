ea8ffcbc2093a7df0e74ac22406f430d05731611
zhangxin <zhangxin8069@qq.com>
2024-11-20 12:41:42 +0800

    2024-11-20-12-41-41

--- ssub.sh
+++ ssub.sh
@@ -7,12 +7,14 @@
 #SBATCH --error=ssub.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zhangxin8069@qq.com
+#SBATCH --reservation=zhangxin_84
 #SBATCH --gres=gpu:8
 #SBATCH -w "gpu025"
+
 # source /public/home/zhangxin/env.sh
 # bash ./make.sh
 # pushd ./test
 # rm log_*
 # mpirun -n 8 python ./test.nccl.bistabcg.qcu-np8.py
 # popd
-sleep 5000
+sleep 70000
