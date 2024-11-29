e9a2e4dc6c9d9971e67574bf19d63ff2f63fc451
zhangxin <zhangxin8069@qq.com>
2024-11-29 16:11:28 +0000

    2024-11-29-16-11-28

--- src/hello_world.cu
+++
@@ -1,9 +0,0 @@
-#include "../include/qcu.h"
-#pragma optimize(5)
-using namespace qcu;
-// #include "../include/include.h"
-// #include "../include/define.h"
-// int main(){
-//     printf("hello world!");
-//     return 0;
-// }
